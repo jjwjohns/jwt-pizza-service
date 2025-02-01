464b4364def476a0033ec2506e57926debbcd3cb
jjwjohns <jordanwjohns@gmail.com>
2025-02-01 16:44:32 -0700

    fixed weird pasting error in tests

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -8,10 +8,7 @@ beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
-<<<<<<< HEAD
   expectValidJwt(testUserAuthToken);
-=======
->>>>>>> 69a44b9208a7560dba6150727b56e15d66a7c927
 });
 
 
@@ -29,7 +26,6 @@ test('register-no-password', async () => {
   expect(regRes.status).toBe(400);
 });
 
-<<<<<<< HEAD
 
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
@@ -44,13 +40,3 @@ test('login', async () => {
 function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
-=======
-test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-
-  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
-  expect(loginRes.body.user).toMatchObject(user);
-});
->>>>>>> 69a44b9208a7560dba6150727b56e15d66a7c927
