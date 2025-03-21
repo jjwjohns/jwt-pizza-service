7140cf571da0f1ecbb566138b00abe504b16f4f0
jjwjohns <jordanwjohns@gmail.com>
2025-03-20 23:05:35 -0600

    first metrics file

---
+++ src/metrics.js
@@ -0,0 +1,107 @@
+const config = require('./config');
+// const os = require('os');
+
+const requests = {};
+
+function requestTracker(req, res, next) {
+    const endpoint = req.path;
+    requests[endpoint] = (requests[endpoint] || 0) + 1;
+    next();
+  }
+
+// function getCpuUsagePercentage() {
+//   const cpuUsage = os.loadavg()[0] / os.cpus().length;
+//   return cpuUsage.toFixed(2) * 100;
+// }
+
+// function getMemoryUsagePercentage() {
+//   const totalMemory = os.totalmem();
+//   const freeMemory = os.freemem();
+//   const usedMemory = totalMemory - freeMemory;
+//   const memoryUsage = (usedMemory / totalMemory) * 100;
+//   return memoryUsage.toFixed(2);
+// }
+
+
+// function sendMetricsPeriodically(period) {
+//     const timer = setInterval(() => {
+//       try {
+//         const buf = new MetricBuilder();
+//         httpMetrics(buf);
+//         systemMetrics(buf);
+//         userMetrics(buf);
+//         purchaseMetrics(buf);
+//         authMetrics(buf);
+  
+//         const metrics = buf.toString('\n');
+//         this.sendMetricToGrafana(metrics);
+//       } catch (error) {
+//         console.log('Error sending metrics', error);
+//       }
+//     }, period);
+//   }
+
+
+// This will periodically send metrics to Grafana
+const timer = setInterval(() => {
+  Object.keys(requests).forEach((endpoint) => {
+    sendMetricToGrafana('requests', requests[endpoint], { endpoint });
+  });
+}, 10000);
+
+function sendMetricToGrafana(metricName, metricValue, attributes) {
+  attributes = { ...attributes, source: config.source };
+
+  const metric = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics: [
+              {
+                name: metricName,
+                unit: '1',
+                sum: {
+                  dataPoints: [
+                    {
+                      asInt: metricValue,
+                      timeUnixNano: Date.now() * 1000000,
+                      attributes: [],
+                    },
+                  ],
+                  aggregationTemporality: 'AGGREGATION_TEMPORALITY_CUMULATIVE',
+                  isMonotonic: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric.resourceMetrics[0].scopeMetrics[0].metrics[0].sum.dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  fetch(`${config.url}`, {
+    method: 'POST',
+    body: JSON.stringify(metric),
+    headers: { Authorization: `Bearer ${config.apiKey}`, 'Content-Type': 'application/json' },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.error('Failed to push metrics data to Grafana');
+      } else {
+        console.log(`Pushed ${metricName}`);
+      }
+    })
+    .catch((error) => {
+      console.error('Error pushing metrics:', error);
+    });
+}
+
+module.exports = { requestTracker };
