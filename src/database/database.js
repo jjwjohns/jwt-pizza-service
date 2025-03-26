4df82ebdaf032938fa3e73c3896fc0e989448ed0
jjwjohns <jordanwjohns@gmail.com>
2025-03-25 22:46:36 -0600

    logging db queries

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const logging = require('../logger.js');
 
 class DB {
   constructor() {
@@ -287,6 +288,7 @@ class DB {
 
   async query(connection, sql, params) {
     const [results] = await connection.execute(sql, params);
+    logging.log(null, "DB", sql);
     return results;
   }
 
